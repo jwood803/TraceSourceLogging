0ce2e34c84fd30f484e027796087edf0c5cdbded
Jon <chrono803@yahoo.com>
2013-08-07 21:37:39 -0400

    Initial project creation
    Using a console app to help demonstrate TraceSource logging

---
+++ TraceSourceLogging/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TraceSourceLogging
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
